b5ef89b795f5a87f89861aafc497892fc581308e
AnaFerreira015 <dasilvaferreira.af@hotmail.com>
2022-05-12 10:03:43 -0300

    Refactor the code

--- examples/planets.cpp
+++ examples/planets.cpp
@@ -10,12 +10,14 @@
 #define GRAY    0.6, 0.6, 0.6
 
 int frame = 0;
-double turningSpeed;
+double turningSpeed = 1.0;
+bool control = false;
 
+// Permite que a animação seja executada
 void doFrame(int value) 
 {
     glutTimerFunc(10, doFrame, 0);
-    frame++;
+    //frame++;
     glutPostRedisplay();
 }
 
@@ -36,32 +38,18 @@ void keyboard(unsigned char key, int x, int y)
 {
     char OPTIONS[] = { 'y', 'Y' };
 
-    if (std::find(std::begin(OPTIONS), std::end(OPTIONS), key))
+    if ((std::find(std::begin(OPTIONS), std::end(OPTIONS), key)) && !control)
     {
         glutTimerFunc(10, doFrame, 1);
+        frame++;
+    }
+    else {
+        control = true;
     }
 }
 
-void planetWithMoons() 
+void moonX()
 {
-    glPushMatrix();
-
-    glColor3f(CYAN);
-    turningSpeed = 0.5;
-    glRotatef(turningSpeed * frame, 0, 1, 0);
-    glTranslatef(-2.5, 0.4, -1);
-    glPushMatrix();
-    glutSolidSphere(0.2, 100, 100);
-    glPopMatrix();
-
-    glPushMatrix();
-    glColor3f(PURPLE);
-    turningSpeed = 2.0;
-    glRotatef(turningSpeed * frame, 1, 1, 0);
-    glTranslatef(0.35, 0, 0);
-    glutSolidSphere(0.07, 100, 100);
-    glPopMatrix();
-
     glPushMatrix();
     glColor3f(GRAY);
     turningSpeed = 1.5;
@@ -69,33 +57,72 @@ void planetWithMoons()
     glTranslatef(0.45, 0.1, 0);
     glutSolidSphere(0.05, 100, 100);
     glPopMatrix();
+}
 
+void moonY()
+{
+    glPushMatrix();
+    glColor3f(PURPLE);
+    turningSpeed = 2.0;
+    glRotatef(turningSpeed * frame, 1, 1, 0);
+    glTranslatef(0.35, 0, 0);
+    glutSolidSphere(0.07, 100, 100);
+    glPopMatrix();
+}
 
+void planetWithMoons()
+{
+    glPushMatrix();
+    glColor3f(CYAN);
+    turningSpeed = 0.5;
+    glRotatef(turningSpeed * frame, 0, 1, 0);
+    glTranslatef(-2.5, 0.4, -1);
+    glPushMatrix();
+    glutSolidSphere(0.2, 100, 100);
+    glPopMatrix();
+}
+
+void mountPlanetWithMoons() 
+{
+    planetWithMoons();
+
+    moonY();
+
+    moonX();
+
+    glPopMatrix();
+}
+
+void sun()
+{
+    glPushMatrix();
+    glColor3f(ORANGE);
+    glutSolidSphere(0.5, 100, 100);
+    glPopMatrix();
+}
+
+void isolatedPlanet()
+{
+    glPushMatrix();
+    glColor3f(GREEN);
+    turningSpeed = 2;
+    glRotatef(turningSpeed * frame, 0, -1, 0);
+    glTranslatef(-1, -0.05, 0);
+    glutSolidSphere(0.1, 100, 100);
     glPopMatrix();
 }
 
 void solarSystem() 
 {
+    sun();
 
-    glPushMatrix();
-    glColor3f(ORANGE);
-    glutSolidSphere(0.5, 100, 100);
-    glPopMatrix();
+    mountPlanetWithMoons();
 
-    planetWithMoons();
-
-    glPushMatrix();
-    glColor3f(GREEN);
-    turningSpeed = 2;
-    glRotatef(turningSpeed * frame, 0, -1, 0);
-    glTranslatef(-1, -0.05, 0); 
-    glutSolidSphere(0.1, 100, 100);
-    glPopMatrix();
+    isolatedPlanet();
 }
 
 void display() 
 {
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glPushMatrix();
@@ -107,7 +134,6 @@ void display()
 
 void init() 
 {
-
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_COLOR_MATERIAL);
@@ -127,7 +153,7 @@ int main(int argc, char** argv) {
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
-    glutReshapeFunc(reshape);
+    glutReshapeFunc(reshape); // Remodela a janela atual
     glutMainLoop();
 
     return 0;
